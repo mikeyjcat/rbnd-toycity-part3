d67e70150e16b06b17cf50de2246eec61cd5524f
mikeyjcat <mike@hewitson.co.za>
2016-01-11 19:28:10 +0200

    Added ability to save the product title, and raise and exception when a duplicate product is added

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
+  attr_reader :title
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_products
   end
 
   def self.all
     @@products
   end
+
+  private
+
+  def add_products
+    @@products.each do |product|
+      if @title == product.title
+        raise DuplicateProductError, "'" + product.title + "' already exists"
+      end
+    end 
+    @@products << self
+  end
+
 end  
