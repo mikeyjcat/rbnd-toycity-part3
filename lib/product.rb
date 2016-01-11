f90bf878963696e218b6aaf22b36774c011fe7ed
mikeyjcat <mike@hewitson.co.za>
2016-01-11 19:52:34 +0200

    Added accessors for price and stock, and the in_stock class method

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,11 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_products
   end
 
@@ -11,13 +13,23 @@ class Product
     @@products
   end
 
+  def self.find_by_title(title)
+    @@products.find { |product| product.title == title } 
+  end
+
+  def in_stock?
+    @stock > 0 ? true : false
+  end
+
+  def self.in_stock
+    @@products.select { |product| product.stock > 0 }
+  end
+
   private
 
   def add_products
-    @@products.each do |product|
-      if @title == product.title
-        raise DuplicateProductError, "'" + product.title + "' already exists"
-      end
+    if Product.find_by_title(@title)
+      raise DuplicateProductError, "'" + @title + "' already exists"
     end 
     @@products << self
   end
